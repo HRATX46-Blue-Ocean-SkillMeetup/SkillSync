d7ce70cfe28dc0152f896c12058467d9b0032cfb
james conant <jamesconant@jamess-MacBook-Pro.local>
2020-03-06 11:24:12 -0600

    dynamiclly rendering mentor and mentees

--- src/components/userProfile/LearnSkill.js
+++ src/components/userProfile/LearnSkill.js
@@ -2,9 +2,42 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function LearnSkill() {
+  const [menteeSkill, setMenteeSkill] = useState([]);
+  const [mentorSkill, setMentorSkill] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/getSkills", {
+        params: {
+          ID: 20
+        }
+      })
+      .then(function(response) {
+        const menteeSkill = [];
+        const mentorSkill = [];
+        response.data.forEach(skill => {
+          if (skill.role === "mentee") {
+            mentorSkill.push(skill.skill);
+          } else {
+            menteeSkill.push(skill.skill);
+          }
+        });
+        setMenteeSkill(menteeSkill);
+        setMentorSkill(mentorSkill);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }, []);
+
+  const testArray = ["one", "two", "three", "four"];
   return (
     <div>
-      <div className="userProfile-learn-skill">sample</div>
+      <div className="userProfile-learn-skill">
+        {menteeSkill.map(x => (
+          <li className="userProfile-learn-skill">{x}</li>
+        ))}
+      </div>
     </div>
   );
 }
