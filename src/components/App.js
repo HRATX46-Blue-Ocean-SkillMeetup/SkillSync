09e40a955e4108414f65b91eafdc8854ba165f3b
agatadrozdowska <agata.drozdowska@icloud.com>
2020-03-06 19:33:26 -0600

    Refactored code

--- src/components/App.js
+++ src/components/App.js
@@ -10,19 +10,7 @@ import NewPostForm from "./NewPostForm";
 import PostingDetailsContainer from "./PostingDetailsContainer";
 
 export function App() {
-  const [postingId, setPostingId] = useState(4);
-  const [photo, setPhoto] = useState(
-    "https://images.unsplash.com/photo-1529736576495-1ed4a29ca7e1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
-  );
-  const [activity, setActivity] = useState("JavaScript");
-  const [mentor, setMentor] = useState("frodriguez");
-  const [postingDescription, setPostingDescription] = useState(
-    "Fugit excepturi et corporis autem possimus. Aut qui minima aliquam dicta eos. Est consequatur aut adipisci iure qui."
-  );
-  const [selectedSkill, setSelectedSkill] = useState({});
-  const [location, setLocation] = useState("53936");
-  const [rating, setRating] = useState(0);
-  const [skills, setSkills] = useState([]);
+  const [postingId, setPostingId] = useState(1);
   const [userId, setUserId] = useState(1);
 
   //submits any input data to the database
@@ -39,53 +27,6 @@ export function App() {
         console.error("--> jeepers: ", err);
       });
   };
-
-  const handleChange = event => {
-    setPostingDescription(event.target.value);
-  };
-
-  const handleSkillChange = event => {
-    setSelectedSkill(event.target.value);
-  };
-
-  const handleClickPost = event => {
-    console.log(selectedSkill);
-    console.log(userId);
-    console.log(postingDescription);
-    axios
-      .post("/addPosting", {
-        skillId: selectedSkill,
-        description: postingDescription,
-        userId: userId
-      })
-      .then();
-  };
-
-  //QUERY FOR INDIVIDUAL POSTING
-  // axios.get(`/postingData/${postingId}`).then(function(response) {
-  //   setPhoto(response.data.user_photo);
-  //   setActivity(response.data.skill);
-  //   setMentor(response.data.username);
-  //   setPostingDescription(response.data.description);
-  //   setLocation(response.data.location);
-  //   setUserId(response.data.user_id);
-
-  //   axios.get(`/user/rating/${userId}`).then(function(response) {
-  //     setRating(response.data[0].rating);
-  //   });
-  // });
-
-  //QUERY FOR NEW POST FORM
-  // axios.get(`/getSkills/${userId}`).then(function(response) {
-  //   const skillsArray = [];
-  //   response.data.forEach(obj => {
-  //     skillsArray.push({
-  //       skill: obj.skill,
-  //       id: obj.skill_id
-  //     });
-  //   });
-  //   setSkills(skillsArray);
-  // });
 
   //initializes the custom form hook
   const { inputs, handleInputChange, handleSubmit } = useForm(submit);
@@ -116,36 +57,22 @@ export function App() {
   //My Bookings
 
   //New Post
-  // <NewPostForm
-  //   skills={skills}
-  //   skillDescription={postingDescription}
-  //   handleChange={handleChange}
-  //   selectedSkill={selectedSkill}
-  //   handleSkillChange={handleSkillChange}
-  //   handleClickPost={handleClickPost}
-  // />
+  //<NewPostForm userId={userId} />
 
   //Search Results
 
   //Individual Posting
-  // <PostingDetailsContainer
-  //     photo={photo}
-  //     activity={activity}
-  //     mentor={mentor}
-  //     location={location}
-  //     rating={rating}
-  //   />
-
+  //<PostingDetailsContainer postingId={postingId} />
   /*Leave a Review*/
 
   return (
     <div>
-      <NavBar
+      {/* <NavBar
         dot={true}
         inputs={inputs}
         handleInputChange={handleInputChange}
         handleSubmit={handleSubmit}
-      />
+      /> */}
     </div>
   );
 }
--- src/components/PostingDetailsContainer.js
+++ src/components/PostingDetailsContainer.js
@@ -1,20 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Location from "./Location.js";
 import Rating from "./Rating.js";
 
 function PostingDetailsContainer(props) {
+  const [photo, setPhoto] = useState(
+    "https://images.unsplash.com/photo-1529736576495-1ed4a29ca7e1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+  );
+  const [activity, setActivity] = useState("");
+  const [mentor, setMentor] = useState("");
+  const [postingDescription, setPostingDescription] = useState(
+    "Fugit excepturi et corporis autem possimus. Aut qui minima aliquam dicta eos. Est consequatur aut adipisci iure qui."
+  );
+  const [location, setLocation] = useState("");
+  const [rating, setRating] = useState();
+
+  useEffect(() => {
+    axios.get(`/postingData/${props.postingId}`).then(function(response) {
+      setPhoto(response.data.user_photo);
+      setActivity(response.data.skill);
+      setMentor(response.data.username);
+      setPostingDescription(response.data.description);
+      setLocation(response.data.location);
+      setRating(response.data.rating);
+    });
+  }, []);
+
   return (
-    <div className="postingDetailsContainer">
-      <img src={props.photo} alt="" className="userPhotoPosting" />
-      <p className="postingTitle">
-        {props.activity} lesson with {props.mentor}
-      </p>
-      <Location location={props.location} />
-      <Rating rating={props.rating} />
-      <p className="postingDescription">
-        I've been playing violin for nearly six decades and teaching for almost
-        three! I'm available most weekdays between 9am and 7pm
-      </p>
+    <div className="posting-postingDetailsContainer">
+      <div className="posting-smallContainer">
+        <div>
+          <img src={photo} alt="" className="posting-userPhotoPosting" />
+        </div>
+        <div>
+          <p className="postingTitle">
+            {activity} lesson with {mentor}
+          </p>
+          <Location location={location} />
+          <Rating rating={rating} />
+        </div>
+      </div>
+      <div>
+        <p className="postingDescription">
+          I've been playing violin for nearly six decades and teaching for
+          almost three! I'm available most weekdays between 9am and 7pm
+        </p>
+      </div>
     </div>
   );
 }
